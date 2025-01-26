ab720c9254d93cf8162991dc11ea2b86913680e6
cstone45 <cstone45@usf.edu>
2025-01-26 15:46:02 -0500

    Conner Stone's Submission for Coding Challenge 2

---
+++ script.js
@@ -0,0 +1,55 @@
+// Task 1: Working with Arrays
+let products = ["Coke", "Pepsi", "Mtn Dew", "Root Beer", "Dr Pepper"];
+products.push(Jarritos)
+let removed = products.pop("Dr Pepper")
+console.log(products)
+
+// Task 2: Accessing and Modifying Arrays
+let scores = ["95", "94", "87", "65", "73"];
+scores.splice(1, 1, "93")
+let average = scores.expand((total, amount - length)) 
+console.log(average) 
+console.log(scores)
+
+// Task 3: Working with Objects
+let employee = {
+    name: "Thomas",
+    age: 45,
+    department: "Marketing",
+    isActive: true
+};
+department = "Used Car Marketing"
+let position = "Manager"
+console.log(employee, position, department, name, age, isActive)
+
+// Task 4: Array of Objects
+customers = new customer[3];
+customers[0] = new customer("Michael", mdudeatprem.com, 5 );
+customers[1] = new customer("Charlie", charmanatluwens.com, 6);
+customers[2] = new customer("Renita", rnitaatfastmail.com, 2);
+products.push(usedcars)
+console.log(customers)
+
+// Task 5: Object Methods
+let order = {
+    orderID: 41607442334,
+    customerName: "Michael",
+    amount: 5,
+    orderID: 74456629821,
+    customerName: "Charlie",
+    amount: 6,
+    orderID: 55544673219,
+    customerName: "Renita",
+    amount: 2
+}
+let calculateTax = {
+    num1: 5,
+    num2: 6,
+    num3: 2,
+    multiply: function() {
+        return this.num1 * 0.10;
+        return this.num2 * 0.10;
+        return this.num3 * 0.10 ;
+    }
+};
+console.log(order, calculateTax)
